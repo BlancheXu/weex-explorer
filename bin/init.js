57644001d352c4c142af7b2b068fd1e6281795ae
许根实 <xugenshi@xiaomi.com>
2017-04-13 11:52:28 +0800

    modify

--- bin/init.js
+++ bin/init.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 require('shelljs/global');
 const shell = require('shelljs');
+const ora = require('ora');
 const ROOT = path.dirname(__dirname);
 const DESTROOT = 'templates/';
 
@@ -11,7 +12,7 @@ const copyDirArr = [
   'assets',
   'build',
   'customized_modules',
-  'node_modules',
+  // 'node_modules',
 ];
 
 const copyFileArr = [
@@ -87,11 +88,13 @@ const dirPromise = new Promise((resolve, reject) => {
       copyDir(ROOT + '/' + item, DESTROOT + val + '/' + item, (err) => {
         if (err) {
           reject(err);
-        } else {
-          resolve();
         }
       })
     })
+
+    if(index == copyDir.length -1) {
+      resolve();
+    }
   });
 })
 
@@ -107,20 +110,39 @@ const filePromise = new Promise((resolve, reject) => {
               shell.exec('cp ' + item + ' ' + DESTROOT + val  + '/' + item, {silent: true}, (code, stdout, stderr) => {
                 if(code != 0) {
                   console.log(stderr);
-                  reject(stderr);
-                } else {
-                  resolve();
-                }
+                } 
+                
               });
             }
         }
       })
     })
+
+    if(index == copyFileArr.length -1) {
+      resolve();
+    }
   })
 })
 
-Promise.all([dirPromise, filePromise], () => {
-  shell.exec('npm run build');
+let spinner = ora(`开始复制文件...\n`).start();
+setTimeout(() => {
+  spinner.color = 'yellow';
+  spinner.text = `正在复制文件, 请稍后...`;
+}, 1000);
+ 
+Promise.all([dirPromise, filePromise]).then(function(data) {
+  spinner.stop();
+  console.log(`复制成功啦！`);
+  // folders.forEach((item, index) => {
+  //   // console.log(`../templates/${item}`)
+  //   // shell.pwd();
+  //   shell.cd(path.resolve(__dirname, '../templates',item));
+  //   shell.exec('npm run build', code => {
+  //     // if(code ==0) continue;
+  //   });
+  // })
 })
+  
+
 
 
